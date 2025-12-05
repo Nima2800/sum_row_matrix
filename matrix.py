540243c6b2e2013092f0ef22b3c91caaecfa103e
Nima <nima12345693@gmail.com>
2025-12-06 01:29:51 +0330

    second commit

--- matrix.py
+++ matrix.py
@@ -1,18 +1,30 @@
 
 def get_matrix(number_row):
-    return [map(int , (input()).split(" ")) for _ in range(number_row)]
+    return [list(map(int , (input()).split(" "))) for _ in range(number_row)]
         
 def calculate_rows(matrix_list):
     return [sum(matrix_list[i]) for i in range(len(matrix_list))]
 
+def calculate_column(matrix_list):
+    return [(matrix_list[0][i] + matrix_list[1][i] +matrix_list[2][i]) for i in range(len(matrix_list))]
 
 if __name__ == "__main__": 
-    list = get_matrix(3)
-    answer = calculate_rows(list)
-    print(f'''Column sums:
+    my_list = get_matrix(3)
+    rows = calculate_rows(my_list)
+    columns = calculate_column(my_list)
+    print(columns)
+    print(f'''Row sums:
 
-Column 1: {answer[0]}
+Row 1: {rows[0]}
 
-Column 2: {answer[1]}
+Row 2: {rows[1]}
 
-Column 3: {answer[2]}''')
+Row 3: {rows[2]}
+          
+Column sums:
+
+Column 1: {columns[0]}
+
+Column 2: {columns[1]}
+
+Column 3: {columns[2]}''')
